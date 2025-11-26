d7a0c2ed97a490e359bd8e218f18ca5b916f51cd
Simone Infante <52280205+infantesimone@users.noreply.github.com>
2025-11-26 11:34:05 +0100

    chore: update config for local dev

--- src/main/kotlin/it/pagopa/selfcare/backoffice/scheduler/PagopaSelfcareBackofficeApplication.kt
+++ src/main/kotlin/it/pagopa/selfcare/backoffice/scheduler/PagopaSelfcareBackofficeApplication.kt
@@ -4,8 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 
-@SpringBootApplication @EnableScheduling
-class PagopaSelfcareBackofficeApplication
+@SpringBootApplication @EnableScheduling class PagopaSelfcareBackofficeApplication
 
 fun main(args: Array<String>) {
     runApplication<PagopaSelfcareBackofficeApplication>(*args)
